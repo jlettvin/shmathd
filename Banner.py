a960498145062c784e2d166db2d709f82f3220b5
Jonathan D. Lettvin <jlettvin@gmail.com>
2015-08-22 14:52:09 -0400

    Improvements to Banner.py

--- Banner.py
+++ Banner.py
@@ -2,40 +2,50 @@
 
 """Banner.py
 Copyright(c) 2012-2015 Jonathan D. Lettvin, All Rights Reserved.
+License: GPLv3 http://www.gnu.org/licenses/gpl-3.0.en.html
 
 Banner is used to output visually useful titles from the command-line.
-Colors are from VT100 legacy support in most terminal emulators.
-0 black
-r red
-g green
-y yellow
-b blue
-m magenta
-c cyan
-w white
-Divider lines are composed from multiple characters (default '***').
 
 Usage:
-    Banner.py \
-[--bare] \
-[--test] \
-[--verbose] \
-[--color=<COLOR>] \
-[--divider=<CHAR>] \
-[--lead=<LEAD>] \
-[--width=<COUNT>] \
-[<args>...]
+    Banner.py [-b] [-c <COLOR>] [-d <CHAR>] [-l <LEAD>] [-w <COUNT>] [<arg>...]
     Banner.py (-h | --help)
+    Banner.py --test
     Banner.py --version
 
 Options:
-    -v, --verbose         Verbose [default: False]
     -b, --bare            Minimized output [default: False]
-    -t, --test            Exhaustive test [default: False]
     -c, --color=<COLOR>   Foreground/background 2 color spec [default: r-]
     -d, --divider=<CHAR>  Char to use when drawing lines [default: *]
     -l, --lead=<LEAD>     Lead char count when drawing lines [default: 3]
-    -w, --width=<COUNT>   Char count to use when drawing lines [default: 70]
+    -w, --width=<COUNT>   Char count to use when drawing lines [default: 0]
+    -t, --test            Exhaustive test [default: False]
+
+bare:    Eliminate the "divider" lines above and below the banner.
+color:   Use a VT100 foreground/background color pair in the banner.
+divider: Use a specific character (like _) to form divider lines (_______)
+lead:    Use a specific length (like 3) on text lines (*** your banner text)
+width:   Use a specific length (like 14) to form divider lines (**************)
+test:    Show example headers for all combinations of output colors.
+
+Default banner is "{login} {datetime} {machine}".
+Divider lines are composed from multiple divider characters.
+Colors are from VT100 legacy support in most terminal emulators.
+    0:black        r:red           g:green         y:yellow
+    b:blue         m:magenta       c:cyan          w:white
+
+Example 1: jlettvin$ ./Banner.py
+*******************************************************************************
+*** jlettvin 2015-08-22T13:42:50.985580 Jonathans-MacBook-Pro.local
+*******************************************************************************
+
+Example 2: jlettvin$ ./Banner.py -d _ -w 30 hello world
+______________________________
+___ hello
+___ world
+______________________________
+
+Example 2: jlettvin$ ./Banner.py -b "Lorem ipsum dolor sit amet"
+*** Lorem ipsum dolor sit amet
 """
 
 from docopt     import (docopt)
@@ -51,6 +61,15 @@ from itertools  import (product)
 from datetime   import (datetime)
 
 version = "Banner.py 1.1.0"
+
+"""
+[--bare] \
+[--color=<COLOR>] \
+[--divider=<CHAR>] \
+[--lead=<LEAD>] \
+[--width=<COUNT>] \
+[<arg>...]
+"""
 
 def columns():
     """
@@ -114,7 +133,7 @@ def outputBanner(**kw):
     xterm escape codes, including those used for color, are found here:
     http://en.wikipedia.org/wiki/ANSI_escape_code
     """
-    if kw.get('verbose'): pprint(kw)
+    #if kw.get('verbose'): pprint(kw)
     colorList = {'0':0, 'r':1, 'g':2, 'y':3, 'b':4, 'm':5, 'c':6, 'w':7}
 
     """Fetch a validated color from kw or use the default white on green."""
@@ -131,8 +150,10 @@ def outputBanner(**kw):
     lead = lead if 0 < lead <= 8 else 3
 
     """Fetch a validated line width from kw or use the default 70."""
-    width = int(kw.get('width', 70))
-    width = width if lead < width <= 80 else 70
+    twidth = columns()
+    width = int(kw.get('width'))
+    width = width if lead<= width <80 else twidth
+    #width = width if lead < width <= 80 else 70
 
     """Fetch a validated line character from kw or use the default *."""
     divider = kw.get('divider', '*')
@@ -152,8 +173,7 @@ def outputBanner(**kw):
 
     """These values enable choice of head and tail."""
     listOfLines = []
-    #listOfLines = ['',]
-    listOfLines += kw['args']
+    listOfLines += kw.get('arg', [])
     listOfLines = listOfLines if listOfLines else [
             getlogin()
             +' '+
@@ -168,14 +188,14 @@ def outputBanner(**kw):
     if bare:
         head = ['', prefix + hue + 'm' + start + ' ']
         tail = ['', prefix + normal + 'm']
-        twidth = columns() - lead - 1 - len(listOfLines[0])
-        fill = ' '*twidth
+        size = twidth - lead - 1 - len(listOfLines[0])
+        fill = '' if size < 0 else ' '*size
         listOfLines[0] = listOfLines[0]+fill
     else:
         head = [start + ' ', prefix + hue + 'm' + divider]
         tail = ['', '\n' + divider+ prefix + normal + 'm']
-        twidth = columns() - len(divider)
-        fill = ' '*twidth
+        size = twidth - len(divider)
+        fill = '' if size < 0 else ' '*size
         head[1] = head[1]+fill+'\n'+start+' '
 
     listLength = len(listOfLines)
@@ -190,12 +210,12 @@ if __name__ == '__main__':
         output = stdout
         print>>output, 'hello'
         # Here is an example using no keywords (all defaults).
-        outputBanner(args=['colorful', 'multihued'])
+        outputBanner(arg=['colorful', 'multihued'])
         # Here is an example using some keywords (some defaults).
-        outputBanner(args=['and', 'useful'], color = 'my', divider = '=', width = 10)
+        outputBanner(arg=['and', 'useful'], color = 'my', divider = '=', width = 10)
         # Here is an example using all keywords.
         outputBanner(
-            args=['gift for all', 'to make banners'],
+            arg=['gift for all', 'to make banners'],
             color = 'ym', divider = '~', lead = 5, width = 80, output = output)
         print>>output, 'world'
         # Now try all possible non-matching color pairs.
@@ -203,10 +223,10 @@ if __name__ == '__main__':
             if color[0] == color[1]: continue
             normal = join(color, '')
             bolder = normal + '!'
-            outputBanner(args=[normal], color = normal)
-            outputBanner(args=[bolder], color = bolder)
-        outputBanner(args=[])
-        outputBanner(args=['bare'], bare=True)
+            outputBanner(arg=[normal], color = normal)
+            outputBanner(arg=[bolder], color = bolder)
+        outputBanner(arg=[])
+        outputBanner(arg=['bare'], bare=True)
 
     def main(**kwargs):
         test() if kwargs.get('test') else outputBanner(**kwargs)
