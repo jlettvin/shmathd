d550de29dce73a4afbcd2a4a7b2130616bd712c0
Jonathan D. Lettvin <jlettvin@gmail.com>
2015-08-25 04:09:01 -0400

    Clarify directory trees

--- Banner/Banner.py
+++ Banner/Banner.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# pylint: disable=W0142
 
 """Banner.py
 Copyright(c) 2012-2015 Jonathan D. Lettvin, All Rights Reserved.
@@ -52,22 +51,22 @@ Example 2: jlettvin$ ./Banner.py --bare "Lorem ipsum dolor sit amet"
 """
 
 ###############################################################################
-from os import (uname, getlogin, environ)
-from sys import (stdout, argv)
-from docopt import (docopt)
-from struct import (unpack)
-from string import (join)
-from platform import (system)
-from datetime import (datetime)
-from itertools import (product)
+from docopt     import (docopt)
+from os         import (uname, getlogin, open, ctermid, O_RDONLY, environ)
+from sys        import (stdout, argv)
+from platform   import (system)
 from subprocess import (check_call)
+from struct     import (unpack)
+from pprint     import (pprint)
+from optparse   import (OptionParser)
+from string     import (join)
+from itertools  import (product)
+from datetime   import (datetime)
 
 ###############################################################################
-VERSION = "Banner.py 1.1.0"
-OSNAME = system()
-TINTLIST = {'0': 0, 'r': 1, 'g': 2, 'y': 3, 'b': 4, 'm': 5, 'c': 6, 'w': 7}
-OPTSLIST = {'b': 'bare', 'c': 'cols', 'd': 'draw', 'l': 'lead', 't': 'tint'}
-
+version = "Banner.py 1.1.0"
+tintList = {'0':0, 'r':1, 'g':2, 'y':3, 'b':4, 'm':5, 'c':6, 'w':7} # VT100
+optsList = {'b':'bare', 'c':'cols', 'd':'draw', 'l':'lead', 't':'tint'}
 
 ###############################################################################
 def columns():
@@ -77,51 +76,48 @@ depends on window width.  Finding that size depends on the OS.
 This code is logically identical to the code found at the following URL:
 stackoverflow.com/questions/566746/how-to-get-console-window-width-in-python
     """
-    cols = 80
-    # _________________________________________________________________________
-    if OSNAME == 'Windows':
+    osname = system()
+    #__________________________________________________________________________
+    if osname == 'Windows':
         # First try screen info resource call.
         try:
-            from ctypes import (windll, create_string_buffer)
-            handle = windll.kernel32.GetStdHandle(-12)  # -11:stdout -12:stderr
-            buf = create_string_buffer(22)
-            resource = windll.kernel32.GetConsoleScreenBufferInfo(handle, buf)
-            if resource:
-                (left, right) = unpack("hhhhHhhhhhh", buf.raw)[5:2]
-                cols = right - left + 1
-        except ImportError:
-            pass
+            from ctypes import windll, create_string_buffer
+        except:
+            return 80
+        handle = windll.kernel32.GetStdHandle(-12) # -11:stdout, -12:stderr
+        buf = create_string_buffer(22);
+        windll.kernel32.GetConsoleScreenBufferInfo(handle, buf)
+        if resource:
+            (bx,by,cx,cy,wa,L,T,R,B,mx,my) = unpack("hhhhHhhhhhh", buf.raw)
+            return R-L+1;
         # If resource fails, try tput, or default to 80.
         try:
-            cols = int(check_call(['tput', 'cols']))
-        except StandardError:
-            pass
-    # _________________________________________________________________________
-    elif OSNAME in ['Linux', 'Darwin'] or OSNAME.startswith('CYGWIN'):
+            return int(check_call(['tput', 'cols']))
+        except:
+            return 80
+    #__________________________________________________________________________
+    elif osname in ['Linux', 'Darwin'] or osname.startswith('CYGWIN'):
         # First try IOCTL calls on stdio handles.
-        for descriptor in (0, 1, 2):
+        for fd in (0,1,2):
             try:
                 from fcntl import (ioctl)
                 from termios import (TIOCGWINSZ)
-                shape = unpack('hh', ioctl(descriptor, TIOCGWINSZ, '1234'))
-                cols = int(shape[1])
-            except ImportError:
+                yx = unpack('hh', ioctl(fd, TIOCGWINSZ, '1234'))
+                return int(yx[1])
+            except:
                 pass
         # If IOCTL fails, try tput, or default to 80
-        if cols == 0:
-            cols = int(environ.get('COLUMNS', 80))
-    # _________________________________________________________________________
+        return int(environ.get('COLUMNS', 80))
+    #__________________________________________________________________________
     else:
         # Neither Windows nor Sys V (unix, Mac, or cygwin).
-        pass
-    return cols
-
+        return 80
 
 ###############################################################################
-def banner(**kw):
+def Banner(**kw):
     """
-    banner outputs a colorful banner for which it is easy to scan.
-    @param kw['tint'] is a color pair from TINTLIST, defaulting to 'wg!'.
+    Banner outputs a colorful banner for which it is easy to scan.
+    @param kw['tint'] is a color pair from tintList, defaulting to 'wg!'.
     @param kw['cols'] is the width of the banner line draw.
     @param kw['draw'] is the single character used in the line draw.
     @param kw['output'] is the output stream to which the banner is written.
@@ -140,52 +136,52 @@ def banner(**kw):
     timestamp = datetime.now().isoformat()
     twidth = columns()              # Get terminal width (see function above).
 
-    # _________________________________________________________________________
-    # Fetch a validated color from kw or use the default white on green.
+    #__________________________________________________________________________
+    """Fetch a validated color from kw or use the default white on green."""
     tint = kw.get('tint', 'g0!')  # See default value for --tint.
     hue = normal = '0'
     bold = int(len(tint) == 3 and tint[2] == '!')
     if len(tint) > 1:
-        if tint[0] in TINTLIST and tint[1] in TINTLIST:
-            hue = '%d;3%d;4%d' % (bold, TINTLIST[tint[0]], TINTLIST[tint[1]])
+        if tintList.has_key(tint[0]) and tintList.has_key(tint[1]):
+            hue = '%d;3%d;4%d' % (bold, tintList[tint[0]], tintList[tint[1]])
 
-    # Fetch a validated lead width from kw or use the default 3.
-    lead = max(3, min(int(kw.get('lead', 8)), 8))  # See --lead default value.
+    """Fetch a validated lead width from kw or use the default 3."""
+    lead = max(3, min(int(kw.get('lead', 8)), 8)) # See --lead default value.
 
-    # Fetch a validated line width from kw or use the default 0.
-    width = max(twidth, min(int(kw.get('cols', 0)), twidth))
+    """Fetch a validated line width from kw or use the default 0."""
+    width = max(twidth, min(int(kw.get('cols',0)), twidth))
 
-    # Fetch a validated line character from kw or use the default @.
+    """Fetch a validated line character from kw or use the default @."""
     draw = kw.get('draw', '@')
     draw = draw if isinstance(draw, str) and len(draw) == 1 else '@'
     start = draw*lead
     divider = draw*width
 
-    # Fetch a validated output stream from kw or use the default sys.stdout.
+    """Fetch a validated output stream from kw or use the default sys.stdout."""
     output = kw.get('output', stdout)
     output = output if isinstance(output, type(stdout)) else stdout
 
-    # This is the standard prefix for xterm escape sequences.
+    """This is the standard prefix for xterm escape sequences."""
     prefix = '\x1b\x5b'
 
-    # The constructed heads and tails for lines for when they are output.
+    """The constructed heads and tails for lines for when they are output."""
     bare = kw.get('bare', False)
 
-    # _________________________________________________________________________
-    # These values enable choice of head and tail.
-    lines = kw.get('arg', [])
-    lined = [getlogin() + ' ' + timestamp + ' ' + list(uname())[1], ]
-    lines = lines if lines else lined
+    #__________________________________________________________________________
+    """These values enable choice of head and tail."""
+    listOfLines = kw.get('arg', [])
+    defaultLine = [getlogin() +' '+ timestamp +' '+ list(uname())[1] ,]
+    listOfLines = listOfLines if listOfLines else defaultLine
 
-    # _________________________________________________________________________
+    #__________________________________________________________________________
     # VT100 emulator doesn't right-fill the first line with correct color.
     # Special case the first line, whether divider or bare.
     if bare:
         head = ['', prefix + hue + 'm' + start + ' ']
         tail = ['', prefix + normal + 'm']
-        size = twidth - lead - 1 - len(lines[0])
+        size = twidth - lead - 1 - len(listOfLines[0])
         fill = '' if size < 0 else ' '*size
-        lines[0] = lines[0]+fill
+        listOfLines[0] = listOfLines[0]+fill
     else:
         head = [start + ' ', prefix + hue + 'm' + divider]
         tail = ['', '\n' + divider + prefix + normal + 'm']
@@ -193,14 +189,13 @@ def banner(**kw):
         fill = '' if size < 0 else ' '*size
         head[1] = head[1] + fill + '\n' + start + ' '
 
-    length = len(lines)
-    final = length - 1
+    listLength = len(listOfLines)
+    lastIndex = listLength - 1
 
-    # _________________________________________________________________________
-    # Output the list of lines with appropriate heads and tails.
-    for index in range(length):
-        print>>output, head[index == 0 or bare] + \
-            lines[index]+tail[index == final]
+    #__________________________________________________________________________
+    """Output the list of lines with appropriate heads and tails."""
+    for index in range(listLength):
+        print>>output, head[index==0 or bare]+listOfLines[index]+tail[index==lastIndex]
 
 ###############################################################################
 if __name__ == '__main__':
@@ -209,9 +204,9 @@ if __name__ == '__main__':
     def command(unit, **kw):
         """command shows what the command-line would look like for **kw."""
         text = 'UNIT TEST %d$ ' % (unit) + argv[0]
-        for key, val in kw.iteritems():
-            key = OPTSLIST.get(key, key)
-            text += ' --'+str(key)+'='+str(val) if key != 'arg' else ''
+        for k,w in kw.iteritems():
+            k = optsList.get(k, k)
+            text += ' --'+str(k)+'='+str(w) if k != 'arg' else ''
         args = kw.get('arg', [])
         if args:
             for arg in args:
@@ -220,63 +215,54 @@ if __name__ == '__main__':
 
     ###########################################################################
     def test1():
-        """Unit test exhaustively exercising some parameters."""
         output = stdout
         print>>output, 'hello'
         # Here is an example using no keywords (all defaults).
-        banner(arg=['colorful', 'multihued'])
+        Banner(arg=['colorful', 'multihued'])
         # Here is an example using some keywords (some defaults).
-        banner(arg=['and', 'useful'], tint='my', draw='=', cols=10)
+        Banner(arg=['and', 'useful'], tint = 'my', draw = '=', cols = 10)
         # Here is an example using all keywords.
-        banner(
+        Banner(
             arg=['gift for all', 'to make banners'],
-            tint='ym', draw='~', lead=5, cols=80, output=output)
+            tint = 'ym', draw = '~', lead = 5, cols = 80, output = output)
         print>>output, 'world'
         # Now try all possible non-matching color pairs.
         for color in list(product('0rgybmcw', repeat=2)):
-            if color[0] == color[1]:
-                continue
+            if color[0] == color[1]: continue
             normal = join(color, '')
             bolder = normal + '!'
-            banner(arg=[normal], tint=normal)
-            banner(arg=[bolder], tint=bolder)
-        banner(arg=[])
-        banner(arg=['bare'], bare=True)
+            Banner(arg=[normal], tint = normal)
+            Banner(arg=[bolder], tint = bolder)
+        Banner(arg=[])
+        Banner(arg=['bare'], bare=True)
 
     ###########################################################################
     def test2():
-        """Unit test showing multi-line message with frame."""
-        args = {'arg': ['hello', 'world'], 'lead': '3'}
+        args = {'arg':['hello', 'world'], 'lead':'3'}
         print command(2, **args)
-        banner(**args)
+        Banner(**args)
 
     ###########################################################################
     def test3():
-        """Unit test showing single-line message without frame."""
-        args = {
-            'arg': ['hello world'], 'lead': '3', 'bare': True, 'tint': 'yb!'}
+        args = {'arg':['hello world'], 'lead':'3', 'bare':True, 'tint':'yb!'}
         print command(3, **args)
-        banner(**args)
+        Banner(**args)
 
     ###########################################################################
-    UNITS = [None, test1, test2, test3]
+    units = [None, test1, test2, test3]
 
     ###########################################################################
-    def main(**kw):
-        """Main for operating Banner.py from the command-line"""
-        unit = int(kw.get('unit', 0))
-        if 0 < unit < len(UNITS):
-            UNITS[unit]()
-        else:
-            banner(**kw)
+    def main(**kwargs):
+        unit = int(kwargs.get('unit', 0))
+        units[unit]() if 0 < unit < len(units) else Banner(**kwargs)
 
     ###########################################################################
-    KWARGS = {
-        k.translate(None, "<->"): v
-        for k, v in
-        docopt(__doc__, version=VERSION).iteritems()}
+    kwargs = {
+            k.replace('-','').replace('<','').replace('>',''): v
+            for k,v in
+            docopt(__doc__,version=version).iteritems()}
 
     ###########################################################################
-    main(**KWARGS)
+    main(**kwargs)
 
 ###############################################################################
---
+++ Banner/README.md
@@ -0,0 +1,3 @@
+Python utility to generate colorized output for vt100 emulator terminals.
+
+This may be used either standalone in bash scripts or as an import for Python scripts.
